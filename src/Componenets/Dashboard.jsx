7182d448dd03e5d29abfb49c772442e0511b0267
SourabhBurman <souravburman55@gmail.com>
2024-02-19 16:55:47 +0530

    dashboard

--- src/Componenets/Dashboard.jsx
+++ src/Componenets/Dashboard.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
 
 function Dashboard() {
+  const [arr,setArray] = useState([]);
+
+    useEffect(()=> {
+       (async ()=> {
+        try{
+             let res = await axios.get('https://precious-wasp-gloves.cyclic.app/pictures',{
+                headers:{
+                    Authorization : `Bearer ${localStorage.getItem('token')}`
+                }
+             });
+             console.log(res.data.album)
+             setArray(res.data.album);
+        } catch(err) {
+            console.log(err);
+        }
+       })()
+    },[])
   return (
-    <div>Dashboard</div>
+    <>
+    <div>Your recent posts</div>
+    {arr.map(item=><div key={item._id}>
+<h2>quote : {item.quote}</h2>
+{/* <p>{item.photo}</p> */}
+<p> device : {item.device}</p>
+<p>comments : {item.commentsCount}</p>
+    </div>)}
+    </>
   )
 }
 
